729b4edcd30972d5084c69866e6ce4283ada11bc
Iryna Burlaka <i.am.blinova@gmail.com>
2024-04-08 09:30:10 +0200

    stydied of loop iteration

--- src/components/City.tsx
+++ src/components/City.tsx
@@ -7,7 +7,6 @@ type IProps = {
 
 export const City = ({ city, onRemoveClick }: IProps) => {
   const handleCityClick = () => onRemoveClick(city);
-  console.log("Element render");
 
   return <div onClick={handleCityClick}>{city}</div>;
 };
---
+++ src/test.js
@@ -0,0 +1,3 @@
+for (let step = 0; step < 10; step++) {
+  console.log(step);
+}
